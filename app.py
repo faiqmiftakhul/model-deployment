327f2701606473d4010e868f2961feef4dae07cf
Faiq Miftakhul Falakh <faiq.mif@gmail.com>
2025-10-08 20:52:36 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+from ml_app import run_ml_app
+
+def main():
+    menu = ['Home', 'Machine Learning']
+    choice = st.sidebar.selectbox('Menu', menu)
+
+    if choice == 'Home':
+        st.subheader('Welcome to homepage')
+    elif choice == 'Machine Learning':
+        st.subheader('Welcome to Prediction Page')
+        run_ml_app()
+
+if __name__ == '__main__':
+    main()
